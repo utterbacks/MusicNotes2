efd0f64ab625017173854d37637b36c4c06850b2
utterbacks <utterbackstephen@gmail.com>
2020-10-12 17:30:28 -0500

    fixing some bugs

--- routes/school.js
+++ routes/school.js
@@ -2,9 +2,11 @@ const   express = require("express"),
         router = express.Router(),
         passport = require("passport"),
         User = require("../models/user"),
+        School = require("../models/school"),
         async = require("async"),
         nodemailer = require("nodemailer"),
-        crypto = require("crypto");
+        crypto = require("crypto"),
+        middleware = require("../middleware");
 
 
 // ADMIN REGISTER FORM
@@ -17,6 +19,26 @@ router.get("/adminRegister", (req, res) => {
 
 router.get("/createSchool", (req, res) => {
     res.render("./schools/create")
+});
+
+router.post('/createSchool', middleware.isLoggedIn, (req, res) => {
+    const   newSchool = {
+                schoolName: req.body.schoolName,
+                admin: {
+                        id: req.user._id,
+                        username: req.user.username
+                    },
+                instruments: [req.body.instruments],
+            }
+         School.create(newSchool, function(err){
+             if(err){
+                 console.log(err);
+                 req.flash("error", "Try that again, please!");
+                 res.redirect("back")
+             } else{
+                 res.redirect("users/:id")
+             }
+         })
 })
 
 module.exports = router
